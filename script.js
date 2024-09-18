af7c29521b67e82f080ba6664cc1d0f47f75fb6e
xiejingyuan <110453023+xiejingyuan@users.noreply.github.com>
2024-09-18 15:16:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,11 @@ arr.push(last)
 console.log(arr.reverse()) 
 
 // 2
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 6, 7, 8, 9, 10];
+arr2.splice(0,1)
+const arr3 = [...arr1, ...arr2];
+
+console.log(arr3)
 
 
